521fcc58f408863bec0c6c4a3d1ad3d387783c6e
Annie Truex <artruex@gmail.com>
2019-08-16 00:36:55 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist 
+  
+  attr_accessor :name, :songs 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}  
+  end
+  
+end
